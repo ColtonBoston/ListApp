689c9fc8c7c06b414e07fd7fd3adac7089c85ddd
Colton Boston <coltonbboston@gmail.com>
2018-01-01 17:07:08 -0600

    add index, new, create, and show routes

--- app.js
+++ app.js
@@ -1,10 +1,36 @@
 var express = require("express"),
-    bodyParser = require("body-parser");
+    bodyParser = require("body-parser"),
+    mongoose = require("mongoose");
 
 var app = express();
 
+mongoose.connect("mongodb://localhost/list_app");
 app.set("view engine", "ejs");
 app.use(bodyParser.urlencoded({extended: true}));
+app.use(express.static(__dirname + "/public"));
+
+var listSchema = new mongoose.Schema({
+  name: String,
+  owner: String,
+  privacy: String,
+  items: [],
+  createdAt: {type: Date, default: Date.now}
+});
+
+var List = mongoose.model("List", listSchema);
+
+// List.create({
+//   name: "Groceries",
+//   owner: "Colton",
+//   privacy: "public",
+//   items: ["bananas", "oats", "strawberries", "bacon", "lactose-free milk"]
+// }, function(err, list){
+//   if (err){
+//     console.log(err);
+//   } else {
+//     console.log(list);
+//   }
+// });
 
 app.get("/", function(req, res){
   res.render("landing");
@@ -26,7 +52,13 @@ var data = [
 ];
 
 app.get("/lists", function(req, res){
-  res.render("lists", {lists: data});
+  List.find({}, function(err, allLists){
+    if (err){
+      console.log(err);
+    } else {
+      res.render("index", {lists: allLists});
+    }
+  });
 });
 
 // New
@@ -34,13 +66,31 @@ app.get("/lists/new", function(req, res){
   res.render("new");
 });
 
+// Create
 app.post("/lists", function(req, res){
-  var list = req.body.list;
+  // var list = req.body.list;
+  //
+  // list.items = list.items.split(",");
+  // console.log(list.items);
+  req.body.list.items = req.body.list.items.split(",");
+  List.create(req.body.list, function(err, list){
+    if (err){
+      console.log(err);
+    } else {
+      res.redirect("/lists");
+    }
+  });
+});
 
-  list.items = list.items.split(",");
-  console.log(list.items);
-  data.push(req.body.list);
-  res.redirect("/lists");
+// Show
+app.get("/lists/:id", function(req, res){
+  List.findById(req.params.id, function(err, foundList){
+    if (err){
+      console.log(err);
+    } else {
+      res.render("show", {list: foundList});
+    }
+  });
 });
 
 app.listen("3000", console.log("ListApp started..."));
