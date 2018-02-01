df618538eaf574be998a63947bce2188238aee52
Colton Boston <coltonbboston@gmail.com>
2018-01-31 21:49:54 -0600

    remove error code from ListItem update route's success redirect

--- routes/listItems.js
+++ routes/listItems.js
@@ -35,7 +35,7 @@ router.put("/lists/:id/listItems/:item_id", middleware.checkListItemOwnership, f
     if (err || !updatedItem){
       res.redirect(404, "/lists/" + req.params.id);
     } else {
-      res.redirect(403, "/lists/" + req.params.id);
+      res.redirect("/lists/" + req.params.id);
     }
   });
 });
