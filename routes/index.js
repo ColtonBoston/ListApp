55e345eb4094746dda5f5689982ca8c00e4c0966
Colton Boston <coltonbboston@gmail.com>
2018-05-23 02:49:20 -0500

    add route to check username availability; change sign up redirects to landing page; fix username regex

--- routes/index.js
+++ routes/index.js
@@ -22,15 +22,15 @@ router.get("/register", function(req, res){
 router.post("/register", function(req, res){
   console.log(req.body.username);
   console.log(regexUsername().test(req.body.username));
-  var newUser = new User({username: req.body.username});
+  var newUser = new User({username: req.body.username, email: req.body.email});
   if (req.body.username.includes(" ") || !regexUsername().test(req.body.username)){
     req.flash("error", "Invalid username.");
-    res.redirect("/register");
+    res.redirect("/");
   } else {
     User.register(newUser, req.body.password, function(err, user){
       if(err){
         req.flash("error", err.message);
-        return res.redirect("/register");
+        res.redirect("/");
       } else {
         passport.authenticate("local")(req, res, function(){
           req.flash("success", "Welcome, " + req.body.username + "!");
@@ -39,6 +39,32 @@ router.post("/register", function(req, res){
       }
     });
   }
+});
+
+// Check if username or email is taken
+router.get("/checkUsers", function(req, res){
+  var username = req.query.username;
+  var re = new RegExp("^" + username + "$", "i");
+  if (username.indexOf("@") === -1){
+    User.findOne({"username": re}, function(err, foundUser){
+      if (!foundUser || err){
+        console.log(err);
+        res.send("User not found.");
+      } else {
+        console.log(foundUser);
+        res.send("That user exists");
+      }
+    });
+  } else {
+    res.send("email");
+  }
+  // User.find({$or: [{"username": req.body.username}, {"email": req.body.email}]}, function(err, foundUser){
+  //   if (err){
+  //     console.log(err);
+  //   } else {
+  //     console.log(foundUser);
+  //   }
+  // });
 });
 
 // Login Routes //
@@ -65,8 +91,8 @@ router.get("/logout", function(req, res){
 
 // Regex allowing 3-20 characters from a-z, A-Z, 0-9, _, and -
 function regexUsername() {
-  return /^[\w\-]{3,20}?/;
-};
+  return /^[\w\-]{3,20}$/;
+}
 
 // Escape special characters for regex
 function regexEscape(str) {
