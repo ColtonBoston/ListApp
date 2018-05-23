90670ca2f9acaa45243256d7fa9066a7c83c1249
Colton Boston <coltonbboston@gmail.com>
2018-05-23 02:46:51 -0500

    add email field to user model

--- models/user.js
+++ models/user.js
@@ -2,6 +2,7 @@ var mongoose = require("mongoose");
 var passportLocalMongoose = require("passport-local-mongoose");
 
 var userSchema = new mongoose.Schema({
+  email: {type: String, unique: true, sparse: true, required: true},
   username: String,
   password: String,
   friends: [
