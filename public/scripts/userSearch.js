9e91448417bd52bdfaf75ff6a3aa008cbb3a4a9e
Colton Boston <coltonbboston@gmail.com>
2018-07-12 15:57:49 -0500

    add timer to wait 500ms after user is done typing to search for friends in header search bar

--- public/scripts/userSearch.js
+++ public/scripts/userSearch.js
@@ -1,38 +1,50 @@
-searchResults = $(".search-results");
+var searchResults = $(".search-results"),
+    searchTimer,
+    searchAjax;
 
 $("#search-bar").on("keyup", function(event){
   var search = this.value.split(" ").join(""),
       usersList = $("#users-list");
 
+  clearTimeout(searchTimer);
+
   if (search !== ""){
-    $.ajax({
-      type: "GET",
-      url: "/users/searchjson/" + search,
-      dataType: "json",
-      success: function(users){
-        if (users.length > 0){
-          // Reset users search list to empty
-          usersList.html("");
-          users.forEach(function(user){
-            var index = currentUser.friends.indexOf(user._id);
-            // If currentUser has this user as a friend, show remove friend button
-            if (currentUser && currentUser.friends.indexOf(user._id) > -1){
-              usersList.append("<li class='list-group-item li-user'><span class='li-user-username'><a href='/users/profile/" + user._id + "'>" + user.username + "</a></span><button class='btn-friend btn btn-danger btn-sm pull-right' type='submit' data-user-id='" + user._id + "'>Remove Friend</button></li>");
-              searchResults.removeClass("hide-search-results");
-            } else if (user._id === currentUser._id){
-              // Do nothing if this user is the currentUser
-            } else {
-              // If this user is not already a friend of the currentUser, show add friend button
-              usersList.append("<li class='list-group-item li-user'><span class='li-user-username'><a href='/users/profile/" + user._id + "'>" + user.username + "</a></span><button class='btn-friend btn btn-primary btn-sm pull-right type='submit' data-user-id='" + user._id + "'>Add Friend</button></li>");
-              searchResults.removeClass("hide-search-results");
-            }
-          });
-        } else {
-          usersList.html("");
-          searchResults.addClass("hide-search-results");
+    searchTimer = setTimeout(function(){
+      searchAjax = $.ajax({
+        type: "GET",
+        url: "/users/searchjson/" + search,
+        dataType: "json",
+        beforeSend: function(){
+          // Cancel the previous ajax request if one exists when a new request is sent
+          if (searchAjax && searchAjax != "ToCancelPrevReq" && searchAjax.readyState < 4){
+            searchAjax.abort();
+          }
+        },
+        success: function(users){
+          if (users.length > 0){
+            // Reset users search list to empty
+            usersList.html("");
+            users.forEach(function(user){
+              var index = currentUser.friends.indexOf(user._id);
+              // If currentUser has this user as a friend, show remove friend button
+              if (currentUser && currentUser.friends.indexOf(user._id) > -1){
+                usersList.append("<li class='list-group-item li-user'><span class='li-user-username'><a href='/users/profile/" + user._id + "'>" + user.username + "</a></span><button class='btn-friend btn btn-danger btn-sm pull-right' type='submit' data-user-id='" + user._id + "'>Remove Friend</button></li>");
+                searchResults.removeClass("hide-search-results");
+              } else if (user._id === currentUser._id){
+                // Do nothing if this user is the currentUser
+              } else {
+                // If this user is not already a friend of the currentUser, show add friend button
+                usersList.append("<li class='list-group-item li-user'><span class='li-user-username'><a href='/users/profile/" + user._id + "'>" + user.username + "</a></span><button class='btn-friend btn btn-primary btn-sm pull-right type='submit' data-user-id='" + user._id + "'>Add Friend</button></li>");
+                searchResults.removeClass("hide-search-results");
+              }
+            });
+          } else {
+            usersList.html("");
+            searchResults.addClass("hide-search-results");
+          }
         }
-      }
-    });
+      });
+    }, 500);
   } else {
     usersList.html("");
     searchResults.addClass("hide-search-results");
